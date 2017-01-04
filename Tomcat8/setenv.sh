6c35236ab6d0d78ec27be2a274b85427190dd91b
Lan <lanhuidong@gmail.com>
2017-01-04 14:59:12 +0800

    修改Linux版tomcat的setenv.sh示例

--- Tomcat8/setenv.sh
+++ Tomcat8/setenv.sh
@@ -1,4 +1,4 @@
 JAVA_HOME=/usr/local/jdk1.8.0_45
-CATALINA_OPTS='-XX:+UseConcMarkSweepGC -XX:MaxGCPauseMillis=500 -XX:+PrintGCDetails -Xloggc:/home/ubuntu/app/tomcat2/logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=1M'
+CATALINA_OPTS='-Xms6G -Xmx6G -XX:PermSize=128M -XX:MaxPermSize=256M -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -Xloggc:$CATALINA_BASE/logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=8M'
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/apr/lib
 export LD_LIBRARY_PATH
